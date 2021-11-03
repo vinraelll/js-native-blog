50b8db6d5bcd1de444d4389b084d2bd8d3059631
vinraelll <53097647+vinraelll@users.noreply.github.com>
2021-11-03 18:21:37 +0200

    added navigation logic, now tabs actually works

--- src/components/navigation.component.js
+++ src/components/navigation.component.js
@@ -2,22 +2,28 @@ import {Component} from '../core/component'
 
 export class NavigationComponent extends Component {
   constructor(id) {
-    super(id) 
+    super(id)
+
+    this.tabs = []
   }
 
   init() {
-    this.$el.addEventListener('click', tabsHandler.bind(this))
+    this.$el.addEventListener('click', tabClickHandler.bind(this))
+  }
+
+  registerTabs(tabs) {
+    this.tabs = tabs
   }
 }
 
-function tabsHandler(e) {
-  e.preventDefault()
+function tabClickHandler(event) {
+  Array.from(this.$el.querySelectorAll('.tab')).forEach(tab => tab.classList.remove('active'))
 
-  Array.from(this.$el.querySelectorAll('.tab')).forEach(tab => {
-    tab.classList.remove('active')
-  })
-
-  if (e.target.classList.contains('tab')) {
-    e.target.classList.add('active')
+  if (event.target.classList.contains('tab')) {
+    event.target.classList.add('active')
   }
+
+  const activeTab = this.tabs.find(t => t.name === event.target.dataset.name)
+  this.tabs.forEach(t => t.component.hide())
+  activeTab.component.show()
 }
